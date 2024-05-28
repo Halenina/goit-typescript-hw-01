a22872bde78af4efe23af10ba0682953f1e5a8d2
Halenina <galenina20032101@gmail.com>
2024-05-29 00:11:01 +0300

    final

--- ts-my-project/src/generics/1.ts
+++ ts-my-project/src/generics/1.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-async function fetchData<T>(url: string): Promise<T> {
+export async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
